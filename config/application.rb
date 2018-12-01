6b19ff9f16b6ee4bc1a5516115b80cd08cc41f29
Augusto Santos <augustodam@hotmail.com>
2018-12-01 20:56:04 -0300

    deploy heroku

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module SpaceManagerUfpa
     config.i18n.enforce_available_locales = false
     config.i18n.available_locales = ["pt-BR"]
     config.i18n.default_locale = :'pt-BR'
-    
+    config.assets.initialize_on_precompile = false
   end
 end
